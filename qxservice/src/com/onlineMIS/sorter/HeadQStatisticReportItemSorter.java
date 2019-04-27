a8546a312407efcd62c2f22cd1702c6d5f36a1e5
admin1506132225\Administrator <fredoxia@hotmail.com>
2019-04-27 23:20:38 +0800

    hp: 修改报表的排序

--- qxservice/src/com/onlineMIS/sorter/HeadQStatisticReportItemSorter.java
+++ qxservice/src/com/onlineMIS/sorter/HeadQStatisticReportItemSorter.java
@@ -5,6 +5,8 @@ import java.util.Comparator;
 import com.onlineMIS.ORM.entity.chainS.report.ChainReportItemVO;
 import com.onlineMIS.ORM.entity.headQ.barcodeGentor.Brand;
 import com.onlineMIS.ORM.entity.headQ.barcodeGentor.ProductBarcode;
+import com.onlineMIS.ORM.entity.headQ.barcodeGentor.Quarter;
+import com.onlineMIS.ORM.entity.headQ.barcodeGentor.Year;
 import com.onlineMIS.ORM.entity.headQ.report.HeadQPurchaseStatisticReportItem;
 import com.onlineMIS.ORM.entity.headQ.report.HeadQStatisticReportItem;
 
@@ -19,7 +21,18 @@ Comparator<HeadQStatisticReportItem> {
 		Brand brand0 = pb0.getProduct().getBrand();
 		Brand brand1 = pb1.getProduct().getBrand();
 		
-		if (brand0.getBrand_ID() != brand1.getBrand_ID()){
+		Year year0 = pb0.getProduct().getYear();
+		Year year1 = pb1.getProduct().getYear();
+		
+		Quarter quarter0 = pb0.getProduct().getQuarter();
+		Quarter quarter1 = pb1.getProduct().getQuarter();
+		
+		
+		if (year0.getYear_ID() != year1.getYear_ID()){
+			return year0.getYear_ID() - year1.getYear_ID();
+		} else if (quarter0.getQuarter_ID() != quarter1.getQuarter_ID()){
+			return quarter0.getQuarter_ID() - quarter1.getQuarter_ID();
+		} else if (brand0.getBrand_ID() != brand1.getBrand_ID()){
 			return brand0.getBrand_ID() - brand1.getBrand_ID();
 		} else {
 			String productCode0 = pb0.getProduct().getProductCode();
